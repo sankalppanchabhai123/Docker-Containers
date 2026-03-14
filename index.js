521ed47b0686fedfeb10b8687252cd839625cd0b
Sankalp Panchabhai <sankalppanchabhai5@gmail.com>
2026-03-14 19:03:30 +0530

    added server to connect docker

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express();
 app.route("/", (req, res) => {
     return res.json({ "hey from docker": "ds" });
 })
-
+app.route("/about", (req, res) => {
+    return res.json({ "hey from docker": "ds" });
+})
+app.route("/app", (req, res) => {
+    return res.json({ "hey from docker": "ds" });
+})
+app.route("/api", (req, res) => {
+    return res.json({ "hey from docker": "ds" });
+})
 app.listen(3000, () => {
     console.log("app is running on port 3000")
 })
